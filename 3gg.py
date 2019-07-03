2215522279af35ac7d882230ac516dfdd632dc74
gurushank <51952188+gurushank@users.noreply.github.com>
2019-07-03 14:45:46 +0530

    Create 3gg.py

---
+++ 3gg.py
@@ -0,0 +1,11 @@
+guru1=int(input())
+k=list(map(int,input().split()))
+b=[]
+for i in range(len(k)):
+    if i==k[i]:
+        b.append(i)
+        
+if len(b)>=1:
+    print(*b)
+else:
+    print(-1)
